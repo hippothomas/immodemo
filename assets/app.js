a26f8cae88c964d667e83665184fe3e61f77b870
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-11-29 10:41:14 +1300

    feat(front): add homepage slider bar

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,20 @@ import './scss/app.scss';
 // Slider on homepage
 // ==================
 import { tns } from "tiny-slider"
+
+let sliderbarDiv = document.getElementById('sliderbar-topoffer');
+const SLIDER_CHILD_COUNT = document.getElementById('topoffers_slider').childElementCount;
+
+/**
+ * Dynamically change slider bar width
+ * @param {int} index
+ * @param {int} count
+ */
+function sliderBarWidth(index, count) {
+    let width = (index / count) * 100;
+    sliderbarDiv.style.width = width + "%";
+}
+// Slider instantiation
 let slider = tns({
     container: '.wrapper_offers',
     items: 3,
@@ -18,5 +32,12 @@ let slider = tns({
     controlsContainer: '.arrows_section',
     prevButton: '.arrow-left',
     nextButton: '.arrow-right',
-    nav: false
+    nav: false,
+    onInit: () => {
+        sliderBarWidth(1, SLIDER_CHILD_COUNT);
+    }
+});
+// Slider bar show current position in slider
+slider.events.on('indexChanged', (i) => {
+    sliderBarWidth(i.displayIndex, i.slideCount);
 });
