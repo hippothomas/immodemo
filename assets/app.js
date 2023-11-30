ad0712f5904efd4feda369d686f3de3ffb507b12
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-01 11:44:11 +1300

    fix(front): js reorganisation [temp]

--- assets/app.js
+++ assets/app.js
@@ -1,63 +1,10 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+// Init
+// ==================
 import './scss/app.scss';
 
-// Slider on homepage
+// Pages
 // ==================
-import { tns } from "tiny-slider"
-
-let sliderbarDiv = document.getElementById('sliderbar-topoffer');
-const SLIDER_CHILD_COUNT = document.getElementById('topoffers_slider').childElementCount;
-
-/**
- * Dynamically change slider bar width
- * @param {int} index
- * @param {int} count
- */
-function sliderBarWidth(index, count) {
-    let width = (index / count) * 100;
-    sliderbarDiv.style.width = width + "%";
-}
-// Slider instantiation
-let slider = tns({
-    container: '.wrapper_offers',
-    items: 3,
-    gutter: 20,
-    controlsContainer: '.arrows_section',
-    prevButton: '.arrow-left',
-    nextButton: '.arrow-right',
-    nav: false,
-    onInit: () => {
-        sliderBarWidth(1, SLIDER_CHILD_COUNT);
-    }
-});
-// Slider bar show current position in slider
-slider.events.on('indexChanged', (i) => {
-    sliderBarWidth(i.displayIndex, i.slideCount);
-});
-
-// References slider on homepage
-// ==================
-// Slider instantiation
-let ref_slider = tns({
-    container: '.references_slider',
-    items: 2,
-    gutter: 20,
-    mouseDrag: true,
-    controls: false,
-    navContainer: '.references_slider_dots',
-    autoplay: true,
-    autoplayButtonOutput: false,
-    slideBy: 2,
-    speed: 1500,
-    autoplayTimeout: 10000
-});
+import './js/pages/home.js';
 
 // Click on newsletter button
 // ==================
---
+++ assets/js/pages/home.js
@@ -0,0 +1,51 @@
+import { tns } from "tiny-slider"
+
+if (document.getElementById('homepage') !== null) {
+    // Slider on homepage
+    // ==================
+    let sliderbarDiv = document.getElementById('sliderbar-topoffer');
+    const SLIDER_CHILD_COUNT = document.getElementById('topoffers_slider').childElementCount;
+
+    /**
+     * Dynamically change slider bar width
+     * @param {int} index
+     * @param {int} count
+     */
+    function sliderBarWidth(index, count) {
+        let width = (index / count) * 100;
+        sliderbarDiv.style.width = width + "%";
+    }
+    // Slider instantiation
+    let slider = tns({
+        container: '.wrapper_offers',
+        items: 3,
+        gutter: 20,
+        controlsContainer: '.arrows_section',
+        prevButton: '.arrow-left',
+        nextButton: '.arrow-right',
+        nav: false,
+        onInit: () => {
+            sliderBarWidth(1, SLIDER_CHILD_COUNT);
+        }
+    });
+    // Slider bar show current position in slider
+    slider.events.on('indexChanged', (i) => {
+        sliderBarWidth(i.displayIndex, i.slideCount);
+    });
+
+    // References slider on homepage
+    // ==================
+    let ref_slider = tns({
+        container: '.references_slider',
+        items: 2,
+        gutter: 20,
+        mouseDrag: true,
+        controls: false,
+        navContainer: '.references_slider_dots',
+        autoplay: true,
+        autoplayButtonOutput: false,
+        slideBy: 2,
+        speed: 1500,
+        autoplayTimeout: 10000
+    });
+}
