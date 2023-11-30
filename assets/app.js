e7f8b5e6e020d2fff9e54476f96fad8834d581fe
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-11-30 19:37:03 +1300

    feat(front): add animations on homepage

--- assets/app.js
+++ assets/app.js
@@ -64,3 +64,16 @@ let ref_slider = tns({
 document.getElementById('newsletter-btn').addEventListener('click', () => {
     alert("Merci de l'intérêt que vous portez à ce site mais c'est un site de démo, tout est donc fictif. Si le site vous plaît, n'hésitez pas à consulter mon site hippolyte-thomas.fr pour plus d'informations.");
 });
+
+// Smooth scrolling to html anchor
+// https://stackoverflow.com/a/7717572/19209643
+// ==================
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
