cffa5d04210289f5f00e1f6616f73ea30baeaa8d
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-03 11:03:03 +1300

    feat(front): add interactive map to property page

--- assets/js/pages/property.js
+++ assets/js/pages/property.js
@@ -1,4 +1,5 @@
 import { tns } from "tiny-slider"
+import L from "leaflet"
 
 if (document.getElementById('property') !== null) {
     // Property Slider
@@ -20,4 +21,19 @@ if (document.getElementById('property') !== null) {
         nextButton: '.arrow-right',
         nav: false
     });
+    // Setting up the map
+    // ==================
+    // Need to reset icon location due to webpack issue
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    });
+    const map = L.map('map').setView([51.5, -0.09], 17);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+    }).addTo(map);
+    L.marker([51.5, -0.09]).addTo(map);
 }
