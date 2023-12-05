746313016282c5f3ce8185631cb300e8912900d8
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-06 10:00:34 +1300

    feat(front): add dynamic data to home & search page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
+use App\Repository\TestimonialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(PropertyRepository $propertyRepository, TestimonialRepository $testimonialRepository): Response
     {
-        return $this->render('pages/home.html.twig');
+        // Fetch featured properties
+        $featuredList = $propertyRepository->findBy(['featured' => true, 'status' => 2], null, 10);
+
+        // Fetch last testimonials
+        $testimonialList = $testimonialRepository->findBy([], ['created' => 'DESC'], 6);
+
+        return $this->render('pages/home.html.twig', [
+            'featured' => $featuredList,
+            'testimonials' => $testimonialList
+        ]);
     }
 }
--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchController extends AbstractController
 {
     #[Route('/recherche', name: 'search')]
-    public function index(): Response
+    public function index(PropertyRepository $propertyRepository): Response
     {
-        return $this->render('pages/search.html.twig');
+        // Fetch last properties
+        $propertiesList = $propertyRepository->findBy(['status' => 2], ['updated' => 'DESC'], 12);
+
+        return $this->render('pages/search.html.twig', [
+            'properties' => $propertiesList
+        ]);
     }
 }
