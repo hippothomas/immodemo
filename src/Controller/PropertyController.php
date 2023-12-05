d8b01d5011d47ffdc1d112d11e0cc0483c2a271d
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-06 10:25:39 +1300

    feat(front): add dynamic data to property page

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
     #[Route('/annonces/{slug}', name: 'property')]
-    public function index(): Response
+    public function index(Property $property): Response
     {
-        return $this->render('pages/property.html.twig');
+        // Check if property is published
+        if ($property->getStatus() !== 2) throw new NotFoundHttpException("This property doesn't exist!");
+
+        return $this->render('pages/property.html.twig', [
+            'property' => $property
+        ]);
     }
 }
