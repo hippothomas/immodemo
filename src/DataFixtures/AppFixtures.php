0e64addd2b468a382ca2f3e9cd4a1079f159348d
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-05 15:43:52 +1300

    feat(fixture): generate properties & their types

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Property;
+use App\Entity\PropertyType;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -22,6 +24,7 @@ class AppFixtures extends Fixture
         $faker = Factory::create('FR-fr');
 
 		// Generating Users
+        $users = [];
         $userData = ['olivier.luile', 'pierre.lotissement', 'marie.maisonette', 'jeanne.duprix', 'alexandra.visite'];
 		foreach ($userData as $username) {
             $identity = explode('.', $username);
@@ -40,7 +43,47 @@ class AppFixtures extends Fixture
             $user->setPosition($faker->jobTitle());
             $user->setPhoto("/images/pages/home/team/{$identity[0]}.jpg");
 			$manager->persist($user);
+            $users[] = $user;
 		}
+
+		// Generating Property Types
+        $types = [];
+        $types_name = ['Appartement', 'Maison', 'Terrain'];
+		foreach ($types_name as $name) {
+            $type = new PropertyType();
+            $type->setName($name);
+            $manager->persist($type);
+            $types[] = $type;
+        }
+
+		// Generating Properties
+        for ($i=0; $i < 30; $i++) {
+            $property = new Property();
+            $property->setReference('ref'.$faker->randomNumber(5, true));
+            $property->setTitle($faker->sentence());
+            $photos = [];
+            for ($y=0; $y < 5; $y++) {
+                $photos[] = 'https://picsum.photos/id/'.$faker->numberBetween(0, 300).'/735/365';
+            }
+            $property->setPhotos($photos);
+            $property->setPrice($faker->randomFloat(2, 80000, 3000000));
+            $property->setType($types[$faker->numberBetween(0, 2)]);
+            $property->setStatus($faker->numberBetween(0, 2));
+            $property->setFeatured($faker->boolean());
+            $property->setAgent($users[$faker->numberBetween(0, 4)]);
+            $property->setRoom($faker->numberBetween(1, 10));
+            $property->setBedroom($faker->numberBetween(1, 10));
+            $property->setLivingArea($faker->randomFloat(2, 10, 10000));
+            $property->setTotalArea($faker->randomFloat(2, 10, 10000));
+            $property->setCity($faker->city());
+            $property->setPostalCode($faker->postcode());
+            $property->setDescription($faker->paragraphs(4, true));
+            $property->setCoordinateX($faker->longitude());
+            $property->setCoordinateY($faker->latitude());
+            $property->setDpe($faker->numberBetween(10, 200));
+            $property->setGes($faker->numberBetween(10, 80));
+            $manager->persist($property);
+        }
 
         $manager->flush();
     }
