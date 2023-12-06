082d205ddaf5cc70b78343246de6c141cc05f805
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-06 15:14:20 +1300

    fix(front): alert issue w/ leaflet controls

--- assets/app.js
+++ assets/app.js
@@ -30,20 +30,19 @@ new Vue({
 });
 
 // Click on newsletter button
-// ==================
 document.getElementById('newsletter-btn').addEventListener('click', demo);
 
 // Smooth scrolling to html anchor
 // https://stackoverflow.com/a/7717572/19209643
-// ==================
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
         let attr = this.getAttribute('href');
         if (attr === '#') {
-            // FIXME: issue with leaflet
-            // demo();
+            // Check if it's the leaflet controls
+            const leafletControls = this.closest(".leaflet-control-zoom");
+            if (!leafletControls) { demo(); }
             return;
         }
 
