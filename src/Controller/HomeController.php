e697f1f3b635df20d8cff231e83cc04f2d5e013b
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-08 10:48:09 +1300

    feat(front): add PropertyType data in search select

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PropertyRepository;
+use App\Repository\PropertyTypeRepository;
 use App\Repository\TestimonialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,8 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(PropertyRepository $propertyRepository, TestimonialRepository $testimonialRepository): Response
+    public function index(PropertyTypeRepository $propertyTypeRepository, PropertyRepository $propertyRepository, TestimonialRepository $testimonialRepository): Response
     {
+        // Fetch property types
+        $propertyTypes = $propertyTypeRepository->findAll();
+
         // Fetch featured properties
         $featuredList = $propertyRepository->findBy(['featured' => true, 'status' => 2], null, 10);
 
@@ -20,6 +24,7 @@ class HomeController extends AbstractController
         $testimonialList = $testimonialRepository->findBy([], ['created' => 'DESC'], 6);
 
         return $this->render('pages/home.html.twig', [
+            'types' => $propertyTypes,
             'featured' => $featuredList,
             'testimonials' => $testimonialList
         ]);
--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PropertyRepository;
+use App\Repository\PropertyTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchController extends AbstractController
 {
     #[Route('/recherche', name: 'search')]
-    public function index(PropertyRepository $propertyRepository): Response
+    public function index(PropertyRepository $propertyRepository, PropertyTypeRepository $propertyTypeRepository): Response
     {
         // Fetch last properties
         $propertiesList = $propertyRepository->findBy(['status' => 2], ['updated' => 'DESC'], 12);
 
+        // Fetch property types
+        $propertyTypes = $propertyTypeRepository->findAll();
+
         return $this->render('pages/search.html.twig', [
-            'properties' => $propertiesList
+            'properties' => $propertiesList,
+            'types' => $propertyTypes
         ]);
     }
 }
