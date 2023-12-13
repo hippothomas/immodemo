2f57539e06e8b25a26fded573282cb15c6b56153
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2023-12-13 13:27:15 +1300

    feat(front): add responsive to about us and references on homepage

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -40,19 +40,49 @@ export function home() {
         sliderBarWidth(i.displayIndex, i.slideCount);
     });
 
+    // References controls
+    // ==================
+    const REF_SLIDER_CHILD_COUNT = document.getElementById('references_slider').childElementCount;
+
+    /**
+     * Dynamically generate dots controls for reference slider
+     */
+    function initReferencesControls(){
+        let childCount = REF_SLIDER_CHILD_COUNT;
+        if (window.innerWidth > 750) {
+            childCount = childCount / 2;
+        }
+        // Reset the controls div
+        document.getElementById('references_controls').innerHTML = '';
+        // Generate the good amount of dots
+        for (let i = 0; i < childCount; i++) {
+            const dot = document.createElement('div');
+            dot.className = 'dot';
+            document.getElementById('references_controls').appendChild(dot);
+        }
+    }
+    initReferencesControls();
+    window.addEventListener("resize", () => initReferencesControls());
+
     // References slider on homepage
     // ==================
     tns({
         container: '.references_slider',
-        items: 2,
+        items: 1,
         gutter: 20,
         mouseDrag: true,
         controls: false,
         navContainer: '.references_slider_dots',
         autoplay: true,
         autoplayButtonOutput: false,
-        slideBy: 2,
+        slideBy: 1,
         speed: 1500,
-        autoplayTimeout: 10000
+        autoplayTimeout: 10000,
+        responsive: {
+            750: {
+                items: 2,
+                slideBy: 2
+            },
+        }
     });
 }
